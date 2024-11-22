e563f7c1daeb7878cae949015b041a762ec15cf3
Amelia Vieira <113919867+vieira-moon@users.noreply.github.com>
2024-11-21 22:30:46 -0300

    Update quantidadeUsuarios.js
    fsdgsdgf

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -20,7 +20,25 @@ async function quantidadeUsuariosPorRede() {
 
     const layout = {
         plot_bgcolor: getCSS('--bg-color'),
-        paper_bgcolor: getCSS('--bg-color')
+        paper_bgcolor: getCSS('--bg-color'),
+        title: {
+            text:'Redes sociais com mais usuários no mundo',
+            x:0,
+            font: {
+                color: getCSS('--primary-color'),
+                family: getCSS("--font"),
+                size: 30
+            }
+        },
+        xaxis:{
+            title:{
+                text: "nome das redes sociais",
+            }  
+        },
+        yaxis{
+
+        }
+    
 
     }
 
